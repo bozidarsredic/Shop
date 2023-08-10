c7c1450720fcd2621fea16720aa23d443f7705b0
bozidarsredic <79918874+bozidarsredic@users.noreply.github.com>
2023-08-10 17:50:50 +0200

    Commit

---
+++ Shop-application-main/Frontend/src/app/app.module.ts
@@ -0,0 +1,69 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthenticationComponent } from './pages/authentication/authentication.component';
+import { LoginComponent } from './pages/authentication/login/login.component';
+import { RegistrationComponent } from './pages/authentication/registration/registration.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AddArticleComponent } from './components/add-article/add-article.component';
+import { HasRoleDirective } from './directives/has-role.directive';
+import { SellerListComponent } from './components/seller-list/seller-list.component';
+import { AllOrdersComponent } from './components/all-orders/all-orders.component';
+import { ShowUserProfileComponent } from './components/show-user-profile/show-user-profile.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { OrderHistoryComponent } from './components/order-history/order-history.component';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { AllArticlesComponent } from './components/all-articles/all-articles.component';
+import { ArticleCardComponent } from './components/article-card/article-card.component';
+import { SellerArticlesComponent } from './components/seller-articles/seller-articles.component';
+import { ArticleSellerCardComponent } from './components/article-seller-card/article-seller-card.component';
+import { ArticleComponent } from './pages/article/article.component';
+import { ActiveOrdersComponent } from './components/active-orders/active-orders.component';
+import { CountdownModule } from 'ngx-countdown';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthenticationComponent,
+    LoginComponent,
+    RegistrationComponent,
+    DashboardComponent,
+    AddArticleComponent,
+    SellerListComponent,
+    AllOrdersComponent,
+    ShowUserProfileComponent,
+    HasRoleDirective,
+    ProfileComponent,
+    NavbarComponent,
+    HomeComponent,
+    OrderHistoryComponent,
+    AllArticlesComponent,
+    ArticleCardComponent,
+    SellerArticlesComponent,
+    ArticleSellerCardComponent,
+    ArticleComponent,
+    ActiveOrdersComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
+    NgxPaginationModule,
+    CountdownModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Shop-application-main/README.md
@@ -0,0 +1,2 @@
+# Shop-application
+Made a simple shoping application using .NET CORE 6 Web Api , angular and MySQL
